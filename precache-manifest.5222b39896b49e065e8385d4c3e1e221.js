1abdc017bb2058ea41f42058c0e26043be37b41b
sottomarino <simone.ambrosi@live.com>
2021-10-24 17:56:49 +0200

    Updates

---
+++ precache-manifest.5222b39896b49e065e8385d4c3e1e221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e1a71eba10c70ba0c2b6bec343ae7a",
+    "url": "/dstorage-master1/index.html"
+  },
+  {
+    "revision": "2ff974eda7961a0d11f6",
+    "url": "/dstorage-master1/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "6bd047a42b0623f5a43e",
+    "url": "/dstorage-master1/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "2ff974eda7961a0d11f6",
+    "url": "/dstorage-master1/static/js/2.384b1cb9.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/dstorage-master1/static/js/2.384b1cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd047a42b0623f5a43e",
+    "url": "/dstorage-master1/static/js/main.ca61179a.chunk.js"
+  },
+  {
+    "revision": "574ac02a5c66375e20f4",
+    "url": "/dstorage-master1/static/js/runtime-main.5fa77b93.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nft-master/precache-manifest.3730c7f4afc8ebbd259979c02abeb7f9.js"
+  "/dstorage-master1/precache-manifest.5222b39896b49e065e8385d4c3e1e221.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nft-master/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dstorage-master1/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
